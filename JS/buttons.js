0945e8a42cf099f13a9e237b99cbe031c44a80e1
freethinkingzen <johnlewis208503@gmail.com>
2021-03-09 12:49:39 -0800

    allowed doubletap on mobile to select

--- JS/buttons.js
+++ JS/buttons.js
@@ -4,6 +4,19 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 const downGlyph = document.getElementById("downGlyph");
+const codingPage = document.getElementById("codingPage");
+
+// Handles mobile double tap
+var tappedTwice = false;
+function tapHandler(event, elem) {
+    if(!tappedTwice) {
+        tappedTwice = true;
+        setTimeout(function() {tappedTwice = false}, 300);
+        return false;
+    }
+    event.preventDefault();
+    elem.scrollIntoView({behavior: 'smooth'});
+}
 
 // Checks for user's color theme preference from previous visits
 if(currentTheme) {
@@ -28,5 +41,17 @@ toggleSwitch.addEventListener('change', function(e) {
 
 // Scroll down to profile when down arrow on landing is clicked
 downGlyph.addEventListener("click", function() {
-    document.getElementById("profile").scrollIntoView({behavior: 'smooth'});
+    profile.scrollIntoView({behavior: 'smooth'});
 });
+
+coding.addEventListener("dblclick", function(e) {
+    e.preventDefault();
+    codingPage.scrollIntoView({behavior: 'smooth'});
+}, false);
+
+coding.addEventListener("mousedown", function(e) {e.preventDefault();}, false);
+
+coding.addEventListener("touchstart", function(e) {
+    tapHandler(e, codingPage);
+});
+
